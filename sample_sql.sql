f20ad8abb7d78340e27ef7ede6d7e761ec13df20
Jonathan Fowler <jon@fowlercs.com>
2020-03-24 19:46:09 -0400

    Add files via upload

---
+++ sample_sql.sql
@@ -0,0 +1,17 @@
+-- This is a basic SQL comment line, it helps with explaining what happens in the code
+-- You can make a single line comment by typing "--" first, then the text
+-- OR you can do a whole block by starting with a slash and asterisk and then ending with an asterisk then slash as seen below
+/* line one
+line two
+line three
+line four */
+
+-- Anyway, here's a basic SQL statement
+
+SELECT
+field1
+,field2
+FROM
+table1
+WHERE
+field1 LIKE '%string%'
